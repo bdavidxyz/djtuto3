c9f48ff155de32181f36ca913ab0f7f1ee7fc8fe
David Boureau <bdavidxyz@gmail.com>
2024-10-18 15:35:55 +0200

    feat: settings changed

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)r$+q(bv)2kz!(^gzit*za^mu%x0-7w_fx9t!%y6e)ua!3exaj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
